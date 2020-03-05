eb64b55b3b45267c38e72a32a2597dac27541e14
lhj <lhj_001@126.com>
2020-03-05 19:25:46 +0800

    Test

--- SpringCloudTest/src/main/java/CEBIT/SpringCloudTest/App.java
+++ SpringCloudTest/src/main/java/CEBIT/SpringCloudTest/App.java
@@ -13,7 +13,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+    	
+    	System.out.println( "Hello World!" );
         SpringApplication.run(App.class, args);
     }
 }
