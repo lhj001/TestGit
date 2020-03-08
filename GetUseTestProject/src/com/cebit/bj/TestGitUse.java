642cc9d7fa5dacbc086b6a9a269210152b1e6875
LiHongJian <lhj_001@126.com>
2020-03-08 16:12:14 +0800

    梳理库和项目的关系

---
+++ GetUseTestProject/src/com/cebit/bj/TestGitUse.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.cebit.bj;
+
+/**
+ * 版本综合信息：$Id$
+ * 最后提交人: $Author$
+ * 最后提交日期: $Date$
+ * SVN版本号: $Rev$
+ * 源文件的URL地址:$URL$
+ * @Description:
+ * @author Administrator  lhj
+ *
+ */
+public class TestGitUse {
+
+	/**
+	 * 光大科技有限公司
+	 * @Description:TODO
+	 * @Title:main
+	 * @author Administrator 
+	 * @param args void
+	 * TestGitUse 2020年3月8日 下午4:00:33
+	 */
+	public static void main(String[] args) {
+			System.out.println("Test Git Use");
+	}
+}
